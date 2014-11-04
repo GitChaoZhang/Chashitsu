9e36b0cf1ddca52f6ef917e8f89d0294b2131096
Chao Zhang <charles.zhangzs@gmail.com>
2014-11-04 02:46:23 -0500

    update the format

--- csapp_2e/c_warmups/sort.c
+++ csapp_2e/c_warmups/sort.c
@@ -10,8 +10,14 @@ int generate_rands(){
     rand_nums=fopen("rand_num_inputs", "w+");
     int rand_list_counter;
     for(rand_list_counter = 0; rand_list_counter < RAND_LIST_NUMBER_LIMIT; rand_list_counter++){
+        fprintf(rand_nums, "line %d:", rand_list_counter);
+
         int rand_list_length =  rand() % RAND_LIST_LENGTH_LIMIT;
-        fprintf(rand_nums, "the number length %d \n", rand_list_length);
+        int rand_num;
+        for(rand_num = 0; rand_num < rand_list_length; rand_num ++){
+           fprintf(rand_nums, " %d", rand()); 
+        }
+        fprintf(rand_nums, "\n\n");
     }
     fclose(rand_nums);
     return 0; 
@@ -20,5 +26,6 @@ int generate_rands(){
 
 int main(){
     generate_rands();
+    quick_sort();
     return 0;
 }
