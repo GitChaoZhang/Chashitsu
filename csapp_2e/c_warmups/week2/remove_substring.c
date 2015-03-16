eb0819d221ed46e3c23231d23982d256b3af4fe3
Chao Zhang <charles.zhangzs@gmail.com>
2015-03-15 22:57:21 -0700

    needs debug

--- csapp_2e/c_warmups/week2/remove_substring.c
+++ csapp_2e/c_warmups/week2/remove_substring.c
@@ -3,14 +3,35 @@
 #include <string.h>
 #define STRING_LMT 80
 
-void remove_str(const char *s1, const char *s2, char * res){
-    
+void remove_str(char *s1, char *s2, char * res){
+    int res_count = 0;
+    int step_fwd;
+    char * s1_head;
+    char * s2_head;
+    s2_head = s2;
+    while(*s1){
+        printf("current char in s1 %c \n", *s1);
+        s1_head = s1;
+        s2 = s2_head;
+        //assume at least one step would be moved forward each time
+        step_fwd = 1;
+        while(*s1++ == *s2++){
+             step_fwd++;
+        }
+        //if s2_iter is not at its ending position
+        if(*s2 != '\0'){
+            int i;
+            for(i = 0; i < step_fwd; i++)
+                *res++ = *s1_head++; 
+        }
+    }
+    *res = '\0';
 }
 
 int main(int argc, const char *argv[]){
     char s1[STRING_LMT];
     char s2[STRING_LMT];
-    char res[STRING_LMT] = "hello world";
+    char res[STRING_LMT];
     scanf("%s", s1);
     scanf("%s", s2);
 
