bd7d9aca2f02461f254cd1345bd08db260990292
Chao Zhang <charles.zhangzs@gmail.com>
2015-03-17 00:28:57 -0700

    ugly fix for pointer arithmic

--- csapp_2e/c_warmups/week2/remove_substring.c
+++ csapp_2e/c_warmups/week2/remove_substring.c
@@ -7,19 +7,24 @@ void remove_str(char *s1, char *s2, char * res){
     int res_count = 0;
     int step_fwd;
     char * s1_head;
-    char * s2_head;
-    s2_head = s2;
+    char * const s2_head = s2;
     while(*s1){
-        printf("current char in s1 %c \n", *s1);
         s1_head = s1;
         s2 = s2_head;
         //assume at least one step would be moved forward each time
         step_fwd = 1;
-        while(*s1++ == *s2++){
-             step_fwd++;
+        while(*s1 == *s2){
+            s1++;
+            s2++;
+            step_fwd++;
+        }
+        if(step_fwd == 1){
+            s1++;
+            s2++;
         }
         //if s2_iter is not at its ending position
-        if(*s2 != '\0'){
+        if( (strlen(s2_head) == 1 && step_fwd == 1) 
+            || (strlen(s2_head) > 1 && *s2 != '\0') ){
             int i;
             for(i = 0; i < step_fwd; i++)
                 *res++ = *s1_head++; 
@@ -34,8 +39,7 @@ int main(int argc, const char *argv[]){
     char res[STRING_LMT];
     scanf("%s", s1);
     scanf("%s", s2);
-
     remove_str(s1, s2, res);
-    printf("%s", res);
+    printf("%s\n", res);
     return 0;
 }
