c8b018ef1a9193c8b0daced23ae15aeefdcf00f4
Chao Zhang <charles.zhangzs@gmail.com>
2015-04-11 22:08:30 -0700

    finalized the local test

--- csapp_2e/c_warmups/week4/linkedlist_reverse.c
+++ csapp_2e/c_warmups/week4/linkedlist_reverse.c
@@ -13,9 +13,10 @@ typedef struct List {
     ListNode *tail;
 } List;
 
-void print_node(ListNode* node);
+void print_node(ListNode* node, int reverse_mode);
 void add_node(List* pList, int number);
 void print_list(List* plist);
+void print_list_reverse(List* plist);
 
 int main(int argc, char const argv[]){
     //read the input num until -1
@@ -30,20 +31,65 @@ int main(int argc, char const argv[]){
     } while (number != -1);
 
     print_list(&input_list);
+    print_list_reverse(&input_list);
     return 0;
 }
 
-void print_node(ListNode* node){
-    if(node != NULL){
-        if(node->next== NULL)
-            printf("%d", node->value);
-        else
-            printf("%d ", node->value);
+void print_node(ListNode* node, int reverse_mode){
+    if(!reverse_mode){
+        if(node != NULL){
+            if(node->next == NULL)
+                printf("%d\n", node->value);
+            else
+                printf("%d ", node->value);
+        }
+    }else{
+        if(node != NULL){
+            if(node->previous == NULL)
+                printf("%d\n", node->value);
+            else
+                printf("%d ", node->value);
+        }
+
     }
 }
 void add_node(List* pList, int number){
     ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+    node->value = number;
+    node->previous = NULL;
+    node->next = NULL;
+
+    // Check for the head node
+    if(pList->head == NULL){
+        pList->head = node; 
+    }
+
+    // Append the node the tail of the list 
+    if(pList->tail){
+        node->previous = pList->tail;
+        (pList->tail)->next = node;
+    }
+    pList->tail = node;
 }
 
 void print_list(List* plist){
+    //node print not in reverse mode
+    int reverse_mode = 0;
+    if(plist && plist->head){
+        ListNode* p;
+        for(p = plist->head; p; p = p->next){
+            print_node(p, reverse_mode);
+        }
+    }
+}
+
+void print_list_reverse(List* plist){
+    //node print not in reverse mode
+    int reverse_mode = 1;
+    if(plist && plist->tail){
+        ListNode* p;
+        for(p = plist->tail; p; p = p->previous){
+            print_node(p, reverse_mode);
+        }
+    }
 }
